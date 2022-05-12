82edbd35d14a35f95edaaf1cbd3eb37b63a6e34d
Quentin <quentin.francas76@gmail.com>
2022-05-12 15:02:17 +0200

    Création des pages JS, index.js codée
    Co-authored-by: brainafesoh <brainafesoh@users.noreply.github.com>

---
+++ front/js/index.js
@@ -0,0 +1,23 @@
+const urlProduits = "http://localhost:3000/api/products";
+
+let produits;
+
+fetch(urlProduits).then(function (reponse) {
+  reponse.json().then((donnees) => {
+    produits = donnees;
+    console.log("produits", produits);
+
+    // Parcourir le tableau des produits a l'aide d'une boucle
+    for (const produit of produits) {
+      document.getElementById("items").innerHTML += `
+			<a href="./product.html?id=${produit._id}">
+				<article>
+					<img src="${produit.imageUrl}" alt="${produit.altTxt}">
+					<h3 class="productName">${produit.name}</h3>
+					<p class="productDescription">${produit.description}</p>
+				</article>
+			</a>`;
+    }
+    // Pour chaque produit qu'on va parcourir, injecter de facon dynamique le petit bout de code commenté
+  });
+});
---
+++ front/js/product.js
@@ -0,0 +1,6 @@
+//récupérer l'Id de l'URL
+
+let idDuProduit = $_id;
+
+const urlProduit = `http://localhost:3000/api/products/${produit._id}`;
+
